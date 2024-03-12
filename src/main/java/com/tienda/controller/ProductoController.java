9f90a34befc01e7ad6a4caf6adbc61c78a77634b
sebasmorales29 <sebasmoralesvega@hotmail.com>
2024-03-11 21:01:59 -0600

    Semana 9
    Practica semana 9

---
+++ src/main/java/com/tienda/controller/ProductoController.java
@@ -0,0 +1,72 @@
+package com.tienda.controller;
+
+
+import com.tienda.domain.Producto;
+import com.tienda.service.CategoriaService;
+import com.tienda.service.ProductoService;
+import com.tienda.service.impl.FirebaseStorageServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/producto")
+public class ProductoController {
+  
+    @Autowired
+    private ProductoService productoService;
+    
+    @Autowired
+    private CategoriaService categoriaService;
+    
+    @GetMapping("/listado")
+    private String listado(Model model) {
+        var productos = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos",productos.size());
+        model.addAttribute("categorias",categorias);
+        return "/producto/listado";
+    }
+    
+     @GetMapping("/nuevo")
+    public String productoNuevo(Producto producto) {
+        return "/producto/modifica";
+    }
+
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String productoGuardar(Producto producto,
+            @RequestParam("imagenFile") MultipartFile imagenFile) {        
+        if (!imagenFile.isEmpty()) {
+            productoService.save(producto);
+            producto.setRutaImagen(
+                    firebaseStorageService.cargaImagen(
+                            imagenFile, 
+                            "producto", 
+                            producto.getIdProducto()));
+        }
+        productoService.save(producto);
+        return "redirect:/producto/listado";
+    }
+
+    @GetMapping("/eliminar/{idProducto}")
+    public String productoEliminar(Producto producto) {
+        productoService.delete(producto);
+        return "redirect:/producto/listado";
+    }
+
+    @GetMapping("/modificar/{idProducto}")
+    public String productoModificar(Producto producto, Model model) {
+        producto = productoService.getProducto(producto);
+        model.addAttribute("producto", producto);
+        return "/producto/modifica";
+    }   
+}
---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,47 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+
+
+@Data
+@Entity
+@Table(name = "producto")
+
+
+
+public class Producto implements Serializable { //serializacion porque se va almacenar ciertos datos en el disco
+
+    @ManyToOne
+    @JoinColumn(name="id_categoria")
+    Categoria categoria;
+    
+    private static final long serialVersionUID = 1L; //para poder hacer el ciclo de la sumatoria de la producto.
+
+    @Id //id producto es la llave de la tabla producto. 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Los valores generados que estrategia usan, identico a la BD 
+    @Column(name = "id_producto") //decir cual es el nombre en la base de datos. Se hace la asociación 
+    private long idProducto;
+    private long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias; 
+    private String rutaImagen;
+    private boolean activo;
+
+    public Producto() {
+    }
+
+    public Producto(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.rutaImagen = rutaImagen;
+        this.activo = activo;
+    }
+
+}
